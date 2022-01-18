0b3f97155962176776b03f5b6df0c64b55064a3f
miga-666 <82037673+miga-666@users.noreply.github.com>
2022-01-19 01:06:52 +0800

    light

--- lightServer.py
+++ lightServer.py
@@ -1,71 +1,55 @@
-#!/usr/bin/env python3
-# NeoPixel library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
-#
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
-
 import time
 from rpi_ws281x import PixelStrip, Color
-import argparse
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-from telegram.ext import CommandHandler, CallbackQueryHandler  # 指令接收 CallbackQuery
-from telegram.ext import PollAnswerHandler
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton  # 互動式按鈕
-import random
-import json
-# 文字轉語音
-from gtts import gTTS
 from os import system
 from flask import Flask, request, make_response, render_template, send_file
-import requests
+import threading
+import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index() :
+    global light_thread
+    print("The pid is ", os.getpid())
     formData = request.args['mode']
     print(formData)
+    light_thread = None
     myKeyWord(formData)
-    return make_response('OK', 200)
+    # return make_response('OK', 200)
 
 # LED strip configuration:
-LED_COUNT = 111        # Number of LED pixels.
-LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_COUNT = 111       
+LED_PIN = 18 
+LED_FREQ_HZ = 800000   
+LED_DMA = 10 
+LED_BRIGHTNESS = 255 
+LED_INVERT = False
+LED_CHANNEL = 0 
 
 iterations = 20
+light_thread = None
 
 def myKeyWord(formData) :
-    result = ["creepy", "relax", "party", "werewolf", "sleep", 
+    global light_thread
+    result = ["creepy", "relax", "party", "startWerewolf", "sleep", 
               "Christmas", "NewYear", "thePurge", "fu-chou-gui", "pao", 
               "clap", "Birthday", "conan", "so", "music",
-              "turnOff", "WTM"]
-    lightMode = [creepy, relax, party, werewolf, sleep, 
+              "turnOff", "WTM", "werewolf", "witch", "predictor",
+              "breakingDawn"]
+    lightMode = [creepy, relax, party, startWerewolf, sleep, 
                 Christmas, newyear, thePurge, slasher, fuck, 
                 fierce, birthday, conan, turnOff, theaterChaseRainbow,
-                turnOff, thePurge]
-
-    # for i in range(len(type)) :
-    #     if (formData == result[i]) :
-    #         lightMode[i]()
-    #         return
-
-    while True:
-    # print("ttttttt" + str(result.index(formData)))
-    # print(type(result.index(formData)))
-        lightMode[result.index(formData)]()
-    return
+                turnOff, thePurge, werewolf, witch, predictor,
+                breakingDawn]
     
-# Define functions which animate LEDs in various ways.
+    
+    light_thread = threading.Thread(target=lightMode[result.index(formData)]())
+    light_thread.start()
+
+    #while(True):
+
+#一個一個點亮
 def colorWipe(color):
-    """Wipe color across display a pixel at a time."""
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, color)
         strip.show()
@@ -73,7 +57,6 @@ def colorWipe(color):
 
 
 def theaterChase(strip, color, wait_ms=50, iterations=10):
-    """Movie theater light style chaser animation."""
     for j in range(iterations):
         for q in range(3):
             for i in range(0, strip.numPixels(), 3):
@@ -85,7 +68,6 @@ def theaterChase(strip, color, wait_ms=50, iterations=10):
 
 
 def wheel(pos):
-    """Generate rainbow colors across 0-255 positions."""
     if pos < 85:
         return Color(pos * 3, 255 - pos * 3, 0)
     elif pos < 170:
@@ -97,7 +79,6 @@ def wheel(pos):
 
 
 def rainbow(strip, wait_ms=20, iterations=1):
-    """Draw rainbow that fades across all pixels at once."""
     for j in range(256 * iterations):
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, wheel((i + j) & 255))
@@ -105,9 +86,7 @@ def rainbow(strip, wait_ms=20, iterations=1):
         time.sleep(wait_ms / 1000.0)
 
 
-# def rainbowCycle(strip, wait_ms=20, iterations=5):
 def rainbowCycle():
-    """Draw rainbow that uniformly distributes itself across all pixels."""
     for j in range(256 * iterations):
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
@@ -115,7 +94,6 @@ def rainbowCycle():
         time.sleep(50 / 1000.0)
 
 
-# def theaterChaseRainbow(strip, wait_ms=50):
 def theaterChaseRainbow(wait_ms=50):
     """Rainbow movie theater light style chaser animation."""
     for j in range(256):
@@ -135,26 +113,23 @@ def oneColor(color):
     strip.show()
 
 def creepy():
-    print("鬼故事")
+    print("creepy")
     # oneColor(Color(0, 255, 0)) #綠
     # time.sleep(2)
     # rainbow
-    for j in range(255 * iterations, 240 * iterations, -1):
+    for j in range(260 * iterations, 250 * iterations, -1):
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, wheel((i + j) & 255))
         strip.show()
         time.sleep(50 / 1000.0)
-    for j in range(240 * iterations, 255 * iterations):
+    for j in range(250 * iterations, 260 * iterations):
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, wheel((i + j) & 255))
         strip.show()
         time.sleep(50 / 1000.0)
 
 def newyear():
-    print("新年")
-    # oneColor(Color(255, 63, 24)) #紅
-    # time.sleep(2)
-    #theaterChase
+    print("newYear")
     for j in range(iterations):
         for q in range(3):
             for i in range(0, strip.numPixels(), 4):
@@ -168,9 +143,7 @@ def newyear():
             strip.show()
 
 def birthday():
-    print("生日")
-    # oneColor(Color(232, 255, 17)) #黃
-    # time.sleep(2)
+    print("Birthday")
     for i in range(0, strip.numPixels(), 2):
         strip.setPixelColor(i, Color(239, 255, 0))
         strip.setPixelColor(i + 1, Color(18, 255, 10))
@@ -183,10 +156,7 @@ def birthday():
     time.sleep(1)
 
 def fuck():
-    print("打炮")
-    # oneColor(Color(255, 17, 160)) #粉
-    # time.sleep(2)
-    # rainbow
+    print("fuck")
     for j in range(120 * iterations, 130 * iterations, 0.5):
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, wheel((i + j) & 255))
@@ -206,10 +176,7 @@ def thePurge():
     time.sleep(2)
 
 def fierce():
-    print("激烈做愛")
-    # oneColor(Color(255, 17, 160)) #粉
-    # time.sleep(2)
-    #theaterChase
+    print("fierce")
     for j in range(iterations):
         for q in range(3):
             for i in range(0, strip.numPixels(), 3):
@@ -222,9 +189,7 @@ def fierce():
             strip.show()
 
 def party():
-    print("派對")
-    # oneColor(Color(249, 255, 38)) #紅
-    # time.sleep(2)
+    print("party")
     for j in range(iterations):
         for q in range(3):
             for i in range(0, strip.numPixels(), 3):
@@ -238,10 +203,7 @@ def party():
                 strip.setPixelColor(i + q, 0)    
 
 def relax():
-    print("放鬆")
-    # oneColor(Color(67, 230, 192)) #藍綠
-    # time.sleep(2)
-    # rainbow
+    print("relax")
     for j in range(210 * iterations, 200 * iterations, -1):
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, wheel((i + j) & 255))
@@ -254,10 +216,7 @@ def relax():
         time.sleep(50 / 1000.0)
 
 def sleep():
-    print("睡覺")
-    # oneColor(Color(100, 255, 236)) #藍
-    # time.sleep(2)
-    # rainbow
+    print("sleep")
     for j in range(50 * iterations, 30 * iterations, -1):
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, wheel((i + j) & 255))
@@ -270,10 +229,7 @@ def sleep():
         time.sleep(300 / 1000.0)
 
 def Christmas():
-    print("聖誕節")
-    # oneColor(Color(255, 9, 0)) #紅
-    # time.sleep(2)
-    #theaterChase
+    print("christmas")
     for j in range(iterations):
         for q in range(3):
             for i in range(0, strip.numPixels(), 3):
@@ -286,11 +242,8 @@ def Christmas():
                 strip.setPixelColor(i + q, 0)
 
 
-def werewolf():
-    print("狼人殺")
-    # oneColor(Color(132, 30, 223)) #紅
-    # time.sleep(2)
-    # rainbow
+def startWerewolf():
+    print("startWerewolf")
     for j in range(50 * iterations, 20 * iterations, -1):
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, wheel((i + j) & 255))
@@ -303,20 +256,18 @@ def werewolf():
             time.sleep(50 / 1000.0)
 
 def slasher():
-    print("夜光閃亮亮復仇鬼")
-    # oneColor(Color(13, 181, 35)) #綠
-    # time.sleep(2)
-    #theaterChase
-    for j in range(iterations):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i + q, Color(0, 171, 255))
-                strip.setPixelColor(i + 1 + q, Color(255, 255, 255))
-            strip.show()
-            time.sleep(500 / 1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i + q, 0)
-            time.sleep(50 / 1000.0)
+    print("slasher")
+    n = 0
+    while n < 20:
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, Color(0, 0, 0))
+        strip.show()
+        time.sleep(1)
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, Color(0, 205, 255))
+        strip.show()
+        time.sleep(1)
+        n = n + 1
 
 
 def conan():
@@ -326,10 +277,27 @@ def conan():
     #colorWipe
     for i in range(0, strip.numPixels(), 2):
         strip.setPixelColor(1, Color(84, 255, 250))
+    strip.show()
     for i in range(strip.numPixels(), ):
         strip.setPixelColor(i, Color(0, 0, 0))
     strip.show()
     time.sleep(600 / 1000.0)
+
+def werewolf():
+    print("werewolf")
+    colorWipe(Color(255, 0, 10))
+
+def witch():
+    print("witch")
+    colorWipe(Color(196, 0, 255))
+
+def predictor():
+    print("predictor")
+    colorWipe(Color(0, 255, 154))
+
+def breakingDawn():
+    print("breakingDawn")
+    colorWipe(Color(239, 255, 0))
 
 # 關燈
 def turnOff():
@@ -345,8 +313,8 @@ def turnOn():
         strip.setPixelColor(i, Color(255, 255, 255))
         strip.show()
         time.sleep(500 / 1000)
-    iterations = 0
-    while iterations < 5:
+    n = 0
+    while n < 10:
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, Color(0, 0, 0))
         strip.show()
@@ -355,7 +323,7 @@ def turnOn():
             strip.setPixelColor(i, Color(255, 255, 255))
         strip.show()
         time.sleep(1)
-        iterations = iterations + 1
+        n = n + 1
 
 def main() :
     # app.run(host='0.0.0.0', port=8081, threaded=True, debug=True)
