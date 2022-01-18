a5a455512f4bc8d0df314846839a0af429d95376
miga-666 <82037673+miga-666@users.noreply.github.com>
2022-01-19 06:37:42 +0800

    speech recognition

--- sendRequest.py
+++ sendRequest.py
@@ -2,15 +2,14 @@ import requests
 import sys
 
 def main(argv):
-    #讀入需要的參數
+    # 讀入需要的參數
     mode = argv[0]   #發光模式
     ip = '192.168.1.82'   #IP
 
 
-    #發出request
+    # 發出request
     command = 'http://' + ip + ':8081/?mode=' + mode
     requests.get(command)
-    # print('Pi server response :', r.text)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
